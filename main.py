b6d3b529fd9e201b57e6f3f225f130f6275d6031
uisha <joshua_emmanuel_g_tipon@dlsu.edu.ph>
2023-08-27 17:28:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 import requests
+from datetime import datetime
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+MY_LAT = 14.268924
+MY_LONG = 121.072060
+
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()["iss_position"]
+#
+# print(data)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
+data = response.json()
+# print(data)
 
-data = response.json()["iss_position"]
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 
-print(data)
+
+time_now = datetime.now()
+
+print(sunrise)
+print(sunset)
+
+print(time_now.hour)
